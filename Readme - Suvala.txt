26f7c636ff85b00ddcf277594d697ca3a3b49015
tsapina <tosapina@hotmail.com>
2013-12-23 18:10:08 +0100

    Suvala login forma

---
+++ Readme - Suvala.txt
@@ -0,0 +1,2 @@
+Evo login forma je sreðena, sutra æu krenuti raditi suèelje jer danas više ne stignem.
+U login samo ubacite php blokove što treba,ovo su samo sreðene forme i dodan "Forgot your password" link.
--- application/views/account/login.php
+++ application/views/account/login.php
@@ -1,10 +1,113 @@
-<form action="<?php echo site_url('/account'); ?>" method="post">
-    <input type="text" name="username">
-    <input type="password" name="password">
-    <input type="submit">
-<?php if(isset($notify) == true): ?>
-    <div>
-        <?php echo $notify; ?>
+<!DOCTYPE html>
+<html>
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  
+           
+
+  <link rel='stylesheet' href='assets/css/fullcalendar.css'>
+<link rel='stylesheet' href='assets/css/datatables/datatables.css'>
+<link rel='stylesheet' href='assets/css/datatables/bootstrap.datatables.css'>
+<link rel='stylesheet' href='assets/scss/chosen.css'>
+<link rel='stylesheet' href='assets/scss/font-awesome/font-awesome.css'>
+<link rel='stylesheet' href='assets/css/app.css'>
+
+  <link href='http://fonts.googleapis.com/css?family=Oswald:300,400,700|Open+Sans:400,700,300' rel='stylesheet' type='text/css'>
+
+  <link href="assets/favicon.ico" rel="shortcut icon">
+  <link href="assets/apple-touch-icon.png" rel="apple-touch-icon">
+  <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <!--[if lt IE 9]>
+    @javascript html5shiv respond.min
+  <![endif]-->
+
+  <title>TaskOrganiser</title>
+
+</head>
+
+<body>
+
+  <script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-42863888-3', 'pinsupreme.com');
+    ga('send', 'pageview');
+
+  </script>
+
+<div class="all-wrapper no-menu-wrapper">
+  <div class="login-logo-w">
+    <div class="logo">
+      <i class="icon-group" class="logo"></i>
+      <span>TaskOrganiser</span>
     </div>
-<?php endif; ?>
-</form>
+  </div>
+  <div class="row">
+    <div class="col-md-4 col-md-offset-4">
+
+      <div class="content-wrapper bold-shadow">
+        <div class="content-inner">
+          <div class="main-content main-content-grey-gradient no-page-header">
+            <div class="main-content-inner">
+            <form action="" role="form">
+              <h3 class="form-title form-title-first"><i class="icon-lock"></i> Login </h3>
+              <div class="form-group">
+                <label>Username</label>
+                <input type="text" class="form-control" placeholder="Enter username">
+              </div>
+              <div class="form-group">
+                <label>Password</label>
+                <input type="password" class="form-control" placeholder="Password">
+              </div>
+              <div class="form-group">
+                <div>
+                	<a href="#">Forgot your password?</a>
+                </div>
+              </div>
+              <div class="form-group">
+                <div class="checkbox">
+                  <label>
+                    <input type="checkbox"> Remember me
+                  </label>
+                </div>
+              </div>
+              <a href="index.html" class="btn btn-primary btn-lg">Sign in</a>
+              <a href="index.html" class="btn btn-link">Cancel</a>
+            </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+
+
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
+<script src='assets/js/jquery.sparkline.min.js'></script>
+<script src='assets/js/bootstrap/tab.js'></script>
+<script src='assets/js/bootstrap/dropdown.js'></script>
+<script src='assets/js/bootstrap/collapse.js'></script>
+<script src='assets/js/bootstrap/transition.js'></script>
+<script src='assets/js/bootstrap/tooltip.js'></script>
+<script src='assets/js/jquery.knob.js'></script>
+<script src='assets/js/fullcalendar.min.js'></script>
+<script src='assets/js/datatables/datatables.min.js'></script>
+<script src='assets/js/chosen.jquery.min.js'></script>
+<script src='assets/js/datatables/bootstrap.datatables.js'></script>
+<script src='assets/js/raphael-min.js'></script>
+<script src='assets/js/morris-0.4.3.min.js'></script>
+<script src='assets/js/for_pages/color_settings.js'></script>
+<script src='assets/js/application.js'></script>
+
+</body>
+
+</html>
