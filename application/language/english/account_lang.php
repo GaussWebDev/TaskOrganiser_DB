464242e4775a66fba0f83280fd5e8d06b224513d
igudelj <arhon1984@gmail.com>
2013-12-26 04:52:35 +0100

    Added labels and messages for Task actions

--- application/language/english/account_lang.php
+++ application/language/english/account_lang.php
@@ -7,6 +7,10 @@ $lang['lbl_lastname'] = "Last name";
 $lang['lbl_address'] = "Address";
 $lang['lbl_mobile'] = "Mobile";
 $lang['lbl_email'] = "Email";
+//Task labels
+$lang['lbl_duration'] = "Duration";
+$lang['lbl_date_start'] = "Starting Date";
+$lang['lbl_comment'] = "Comment";
 
 $lang['err_required'] = "All fields are required";
 $lang['err_user_pwd'] = "Wrong username or password";
@@ -19,12 +23,21 @@ $lang['msg_new_user_fail'] = "Failed to create new user";
 $lang['msg_new_user_success'] = "New user created successfully";
 $lang['msg_new_account'] = "New account has been created for you on %s. You can login with:\nUsername: %s\nPassword: %s";
 $lang['msg_subject_new_account'] = "Account opened";
+//Insert Task labels
+$lang['msg_new_task_fail'] = "Failed to create new task";
+$lang['msg_new_task_success'] = "New task created successfully";
 
 $lang['msg_update_user_success'] = "User updated successfully";
 $lang['msg_update_user_fail'] = "User update failed";
+//Update Task labels
+$lang['msg_update_task_success'] = "Task updated successfully";
+$lang['msg_update_task_fail'] = "Task update failed";
 
 $lang['msg_project_add_fail'] = "Unable to add project";
 $lang['msg_project_add_success'] = "New project has been added";
 $lang['err_empty_title'] = "Project must have title";
+//Delete Task labels
+$lang['msg_delete_task_success'] = "Task deleted successfully";
+$lang['msg_delete_task_fail'] = "Failed to delete Task";
 
 $lang['msg_project_update_success'] = "Project updated successfully";
