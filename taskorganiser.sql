26f7a5385696d90236679796f8de756dee683126
igudelj <arhon1984@gmail.com>
2013-12-27 02:30:16 +0100

    table project renamed to projects (plural) to synchronise with the remaining tables.
    added the table domain.

--- taskorganiser.sql
+++ taskorganiser.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 22, 2013 at 02:48 PM
+-- Generation Time: Dec 27, 2013 at 02:22 AM
 -- Server version: 5.6.14
 -- PHP Version: 5.5.6
 
@@ -19,6 +19,23 @@ SET time_zone = "+00:00";
 --
 -- Database: `taskorganiser`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `domains`
+--
+
+CREATE TABLE IF NOT EXISTS `domains` (
+  `ID_domain` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `ID_user_fk` int(11) DEFAULT NULL,
+  `domain` text NOT NULL,
+  `date_time_start` datetime NOT NULL,
+  `date_time_expires` datetime DEFAULT NULL,
+  `cost` decimal(10,0) DEFAULT NULL,
+  `expired` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`ID_domain`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -54,16 +71,24 @@ CREATE TABLE IF NOT EXISTS `posts` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `project`
+-- Table structure for table `projects`
 --
 
-CREATE TABLE IF NOT EXISTS `project` (
+CREATE TABLE IF NOT EXISTS `projects` (
   `ID_project` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `title` text NOT NULL,
   `finished` tinyint(1) NOT NULL,
   `date_time_start` datetime NOT NULL,
   PRIMARY KEY (`ID_project`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `projects`
+--
+
+INSERT INTO `projects` (`ID_project`, `title`, `finished`, `date_time_start`) VALUES
+(1, 'testprojekt1', 0, '2013-12-24 00:00:00'),
+(2, 'testprojekt2', 0, '2013-11-15 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -75,6 +100,13 @@ CREATE TABLE IF NOT EXISTS `project_assignees` (
   `ID_project_fk` int(11) NOT NULL,
   `ID_user_fk` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `project_assignees`
+--
+
+INSERT INTO `project_assignees` (`ID_project_fk`, `ID_user_fk`) VALUES
+(1, 2);
 
 -- --------------------------------------------------------
 
@@ -88,7 +120,16 @@ CREATE TABLE IF NOT EXISTS `roles` (
   `permission` int(11) NOT NULL,
   PRIMARY KEY (`ID_role`),
   UNIQUE KEY `permission` (`permission`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
+
+--
+-- Dumping data for table `roles`
+--
+
+INSERT INTO `roles` (`ID_role`, `role`, `permission`) VALUES
+(1, 'Administrator', 100),
+(2, 'Developer', 50),
+(3, 'Client', 1);
 
 -- --------------------------------------------------------
 
@@ -105,7 +146,18 @@ CREATE TABLE IF NOT EXISTS `tasks` (
   `comment` text,
   `billable` tinyint(1) NOT NULL,
   PRIMARY KEY (`ID_task`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=7 ;
+
+--
+-- Dumping data for table `tasks`
+--
+
+INSERT INTO `tasks` (`ID_task`, `ID_user_fk`, `ID_project_fk`, `duration`, `date_start`, `comment`, `billable`) VALUES
+(1, 2, 1, '04:00:00', '2013-12-24', 'test task 1', 0),
+(2, 2, 1, '06:00:00', '2013-12-17', 'test task 2', 1),
+(3, 2, 2, '02:00:00', '2013-12-12', 'test task 3', 1),
+(5, 2, 1, '00:01:00', '2011-08-10', 'update test 1', 0),
+(6, 2, 1, '00:00:08', '2013-10-10', 'asdasdasdasdsadasdasdasdasdasdasd', 1);
 
 -- --------------------------------------------------------
 
@@ -155,7 +207,15 @@ CREATE TABLE IF NOT EXISTS `users` (
   `ID_role_fk` int(11) NOT NULL,
   `active` tinyint(1) NOT NULL,
   PRIMARY KEY (`ID_user`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`ID_user`, `username`, `password`, `firstname`, `lastname`, `address`, `mobile`, `email`, `ID_role_fk`, `active`) VALUES
+(1, 'admin', '$2y$11$adminadminadminadminaOxMpwoIi7VP157XWVBS2ZBQGlY8pvPMm', 'pero', 'peric', 'Osijek, pericpericova 15', '+385989359497', 'peroperic@gmail.com', 1, 1),
+(2, 'markovic', '$2y$11$markovicmarkovicmarkoufkFpGQXvHKjBueUv0MDrh3cVv74zLku', 'marko', 'markovic', 'asdasdasd', '3456436346', 'markovic@markovic.com', 2, 1);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
