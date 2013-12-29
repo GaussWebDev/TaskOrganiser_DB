94b94e490b2849e60e9a2d67185428ae17a8e4bb
igudelj <arhon1984@gmail.com>
2013-12-29 18:01:44 +0100

    view containing table list of all of the domains. allows for updateing and deleting of the listed domains.

---
+++ application/views/admin/domains_list.php
@@ -0,0 +1,45 @@
+<html>
+	<head>
+		<title>Domain List Form</title>
+	</head>
+	<body>
+		<div>
+			<?php $this->load->view('nav'); ?>	
+		</div>		
+		<div>
+			<table>
+				<thead>
+					<tr>
+						<th><?php echo lang('lbl_username'); ?></th>
+						<th><?php echo lang('lbl_domain'); ?></th>
+						<th><?php echo lang('lbl_date_time_start'); ?></th>
+						<th><?php echo lang('lbl_date_time_expires'); ?></th>
+						<th><?php echo lang('lbl_cost'); ?></th>
+						<th><?php echo lang('lbl_expired'); ?></th>
+						<td>&nbsp;</td>
+						<td>&nbsp;</td>
+					</tr>
+				</thead>
+				<tbody>
+<?php foreach ($domains as $domain): ?>	
+					<tr>
+						<td><?php echo $domain['username'];  ?></td>
+						<td><?php echo $domain['domain'];  ?></td>
+						<td><?php echo $domain['date_time_start']; ?></td>
+						<td><?php echo $domain['date_time_expires']; ?></td>
+						<td><?php echo $domain['cost']; ?></td>						
+						<td><input type="checkbox" name="" value="" disabled="disabled" <?php if($domain['expired'] == 1) {?>checked="checked"<?php }?>/></td>
+						<td><a href="<?php echo site_url('domains/edit'), '/', $domain['ID_domain']; ?>"><?php echo lang('lbl_update'); ?></a></td>
+						<td><a href="<?php echo site_url('domains/delete'), '/', $domain['ID_domain']; ?>"><?php echo lang('lbl_delete'); ?></a></td>
+					</tr>
+<?php endforeach ?>
+				</tbody>
+			</table>
+		</div>
+<?php if(isset($notify) == true): ?>
+    	<div>
+			<?php echo $notify; ?>
+    	</div>
+<?php endif; ?>
+	</body>
+</html>
