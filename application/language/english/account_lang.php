d5b19ed88885df08cc003948552f27bdfa162ef5
igudelj <arhon1984@gmail.com>
2013-12-29 18:00:38 +0100

    added messages and labels associated with the domains.
    Corrected few comments regarding tasks, for consistency purposes

--- application/language/english/account_lang.php
+++ application/language/english/account_lang.php
@@ -11,6 +11,15 @@ $lang['lbl_email'] = "Email";
 $lang['lbl_duration'] = "Duration";
 $lang['lbl_date_start'] = "Starting Date";
 $lang['lbl_comment'] = "Comment";
+$lang['lbl_billable'] = "Billable";
+$lang['lbl_update'] = "Update";
+$lang['lbl_delete'] = "Delete";
+//Domain labels
+$lang['lbl_domain'] = "Domain";
+$lang['lbl_date_time_start'] = "Starting date";
+$lang['lbl_date_time_expires'] = "Expiration date";
+$lang['lbl_cost'] = "Cost";
+$lang['lbl_expired'] = "Expired";
 
 $lang['err_required'] = "All fields are required";
 $lang['err_user_pwd'] = "Wrong username or password";
@@ -23,22 +32,31 @@ $lang['msg_new_user_fail'] = "Failed to create new user";
 $lang['msg_new_user_success'] = "New user created successfully";
 $lang['msg_new_account'] = "New account has been created for you on %s. You can login with:\nUsername: %s\nPassword: %s";
 $lang['msg_subject_new_account'] = "Account opened";
-//Insert Task labels
-$lang['msg_new_task_fail'] = "Failed to create new task";
-$lang['msg_new_task_success'] = "New task created successfully";
+//Insert Task messages
+$lang['msg_new_task_fail'] = "Failed to create a new task";
+$lang['msg_new_task_success'] = "A new task was created successfully";
+//Insert Domaim messages
+$lang['msg_new_domain_fail'] = "Failed to create a new domain";
+$lang['msg_new_domain_success'] = "A new domain was created successfully";
 
 $lang['msg_update_user_success'] = "User updated successfully";
 $lang['msg_update_user_fail'] = "User update failed";
-//Update Task labels
+//Update Task messages
 $lang['msg_update_task_success'] = "Task updated successfully";
 $lang['msg_update_task_fail'] = "Task update failed";
+//Update Domain messages
+$lang['msg_update_domain_success'] = "Domain updated successfully";
+$lang['msg_update_domain_fail'] = "Domain update failed";
 
 $lang['msg_project_add_fail'] = "Unable to add project";
 $lang['msg_project_add_success'] = "New project has been added";
 $lang['err_empty_title'] = "Project must have title";
-//Delete Task labels
+//Delete Task messages
 $lang['msg_delete_task_success'] = "Task deleted successfully";
 $lang['msg_delete_task_fail'] = "Failed to delete Task";
+//Delete Domain messages
+$lang['msg_delete_domain_success'] = "Domain deleted successfully";
+$lang['msg_delete_domain_fail'] = "Failed to delete a Domain";
 
 $lang['msg_project_update_success'] = "Project updated successfully";
 
