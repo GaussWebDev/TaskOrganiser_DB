92b45264202f224103b39fdbf0154581159dbb37
igudelj <arhon1984@gmail.com>
2013-12-29 18:02:09 +0100

    corrected the title display

--- application/views/task/add.php
+++ application/views/task/add.php
@@ -1,6 +1,6 @@
 <html>
 	<head>
-		<title>Task Form</title>
+		<title>Task Insert Form</title>
 	</head>
 	<body>
 		<div>
@@ -8,10 +8,10 @@
 		</div>
 		<div>
 			<form action="<?php if (isset($task['ID_task']) == false) {echo site_url('task/add');} else {echo site_url('task/edit'), '/', $task['ID_task'];} ?>" method="post">
-				<p>Duration(seconds): <input type="time" name="duration" value="<?php if(isset($task['duration']) == TRUE) echo $task['duration']; ?>"></p>
-				<p>Starting date: <input type="date" name="date_start" value="<?php if(isset($task['date_start']) == TRUE) echo $task['date_start']; ?>"></p>
-				<p>Comment: <textarea name="comment"><?php if(isset($task['comment']) == TRUE) echo $task['comment']; ?></textarea></p>
-				<p>Billable<input type="checkbox" name="billable" <?php if(isset($task['billable']) == TRUE) {if($task['billable'] == 1) {echo 'checked="checked"';}} ?> /></p>
+				<p><?php echo lang('lbl_duration'); ?>(hh:mm:ss): <input type="time" name="duration" value="<?php if(isset($task['duration']) == TRUE) echo $task['duration']; ?>"></p>
+				<p><?php echo lang('lbl_date_start'); ?>: <input type="date" name="date_start" value="<?php if(isset($task['date_start']) == TRUE) echo $task['date_start']; ?>"></p>
+				<p><?php echo lang('lbl_comment'); ?>: <textarea name="comment"><?php if(isset($task['comment']) == TRUE) echo $task['comment']; ?></textarea></p>
+				<p><?php echo lang('lbl_billable'); ?><input type="checkbox" name="billable" <?php if(isset($task['billable']) == TRUE) {if($task['billable'] == 1) {echo 'checked="checked"';}} ?> /></p>
 				<p><input type="submit"></p>
 			</form>
 		</div>
