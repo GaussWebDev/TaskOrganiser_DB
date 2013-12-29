8edebe1ecf59ba87db913c86d601dd53c2edd15b
igudelj <arhon1984@gmail.com>
2013-12-29 22:15:23 +0100

    model containing actions for the todo page towards the database

--- application/models/domain_model.php
+++ application/models/domain_model.php
@@ -2,14 +2,13 @@
 
 class Domain_model extends CI_Model
 {
-	//TODO: Edit function
 	/**
 	 * Gets all the domains
 	 * 
 	 * @return: returns an array of results
 	 */
 	public function getDomain() {
-		//get the tasks of that user on the current project
+		//get the domains 
 		$query = $this->db->query("SELECT d.*, CONCAT (u.username, ' ', u.firstname, ' ', u.lastname) AS username
 									FROM domains d 
 									LEFT JOIN users u ON u.ID_user = d.ID_user_fk"); 
@@ -19,6 +18,23 @@ class Domain_model extends CI_Model
 
 	
 	
+	/**
+	 * Checks the expiration date on all of the domains 
+	 */
+	 public function checkDomains() {
+	 	//get the domains 
+		$query = $this->db->query("SELECT domain
+									FROM domains
+									WHERE DATEDIFF(date_time_expires, CURDATE()) < 7 AND
+									DATEDIFF(date_time_expires, CURDATE()) > 0 AND
+									expired = 0 OR 
+									expired = NULL;"); 
+						
+		return $query->result_array();
+	 }
+	 
+	 
+	 
 	/**
 	 * adds a new domain
 	 * 
