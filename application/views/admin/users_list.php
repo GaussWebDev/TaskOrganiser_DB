299238d29a1da1efae8782f342ad60a8f5f7d63d
tsapina <tosapina@hotmail.com>
2014-01-03 18:59:41 +0100

    test
    test

--- application/views/admin/users_list.php
+++ application/views/admin/users_list.php
@@ -1,3 +1,32 @@
 <div class="main-content">
-<?php var_dump($users); ?>
+<?php 
+echo "<pre>";
+var_dump($users); 
+echo "</pre>";
+
+?>
+
+<table class="table table-striped table-bordered table-hover datatable">
+<thead>
+	<tr>
+	  <th>Username</th>
+	  <th>Firstname</th>
+	  <th>Lastname</th>
+	  <th>Address</th>
+	  <th>Mobile</th>
+	  <th>Email</th>
+	  <th>Role</th>
+	</tr>
+</thead>
+<tbody>
+<?php 
+foreach ($users as $user) 
+{
+	echo "<th>{$user['username']}</th>";
+
+}
+
+?>
+</tbody>
+</table>
 </div>
