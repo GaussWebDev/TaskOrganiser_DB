c06a9662f5858ab0feceaa8c11c07f6b7a548dc0
tsapina <tosapina@hotmail.com>
2014-01-03 18:12:12 +0100

    ucfirst and dashboard

--- application/views/dashboard/admin.php
+++ application/views/dashboard/admin.php
@@ -2,6 +2,11 @@
 <div class="hero-unit">
 <h1 style="margin-top:-20px;">Welcome to TaskOrganiser!</h1>
 <h3>This is the administrator panel.</h3> 
-<br><br> <h1><?php echo $this->User_model->getFullName();?></h1>
+<br><br> <h1>
+<?php 
+echo ucfirst($this->User_model->getFirstName());
+echo " ";
+echo ucfirst($this->User_model->getLastName());
+?></h1>
 </div>
 </div>
--- application/views/dashboard/default.php
+++ application/views/dashboard/default.php
@@ -1,3 +1,5 @@
+<div class="main-content">
 <p><?php echo $name; ?>, normal user</p>
 
 <?php $this->load->view('nav'); ?>
+</div>
