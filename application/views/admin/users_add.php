3999686f38a5823d70b6b7d0e7800171c1ef1173
Stjepan Udovičić <udovicic.stjepan@gmail.com>
2014-01-04 00:22:19 +0100

    Suvala work #1

--- application/views/admin/users_add.php
+++ application/views/admin/users_add.php
@@ -1,28 +1,41 @@
 <div class="main-content">
-<?php if (isset($user['ID_user']) == false): ?>
-<form action="<?php echo site_url('users/add'); ?>" method="post">
-<?php else: ?>
-<form action="<?php echo site_url('users/edit'), '/', $user['ID_user']; ?>" method="post">
-<?php endif; ?>
-    <p>Username: <input type="text" name="username" value="<?php if (isset($user['username']) == true) echo $user['username']; ?>"></p>
-    <p>First name: <input type="text" name="firstname" value="<?php if (isset($user['firstname']) == true) echo $user['firstname']; ?>"></p>
-    <p>Last name: <input type="text" name="lastname" value="<?php if (isset($user['lastname']) == true) echo $user['lastname']; ?>"></p>
-    <p>Address: <input type="text" name="address" value="<?php if (isset($user['address']) == true) echo $user['address']; ?>"></p>
-    <p>Mobile: <input type="text" name="mobile" value="<?php if (isset($user['mobile']) == true) echo $user['mobile']; ?>"></p>
-    <p>Email: <input type="text" name="email" value="<?php if (isset($user['email']) == true) echo $user['email']; ?>"></p>
-<?php if (isset($user['ID_user']) == true): ?>
-    <p>Active: <input type="checkbox" name="active" value="1" <?php if ($user['active']==1) echo 'checked="checked"'; ?>></p>
-<?php endif; ?>
-    <p>
-        Role:
-        <select name="role" id="">
-<?php foreach ($roles as $role): ?>
-            <option value="<?php echo $role['ID_role']; ?>"><?php echo $role['role']; ?></option>
-<?php endforeach; ?>
-        </select>
-    </p>
-    <p><input type="submit"></p>
-</form>
+
+    <div class="add-form">
+
+
+            <?php if (isset($user['ID_user']) == false): ?>
+            <form action="<?php echo site_url('users/add'); ?>" method="post">
+            <?php else: ?>
+            <form action="<?php echo site_url('users/edit'), '/', $user['ID_user']; ?>" method="post">
+            <?php endif; ?>
+                <div class="form-group">
+                        <label>Username:</label> <input placeholder="Username" class="form-control" type="text" name="username" value="<?php if (isset($user['username']) == true) echo $user['username']; ?>"></div>
+                        <div class="form-group"><label>First name:</label> <input placeholder="First name" class="form-control" type="text" name="firstname" value="<?php if (isset($user['firstname']) == true) echo $user['firstname']; ?>"></div>
+                        <div class="form-group"><label>Last name:</label> <input placeholder="Last name" class="form-control" type="text" name="lastname" value="<?php if (isset($user['lastname']) == true) echo $user['lastname']; ?>"></div>
+                        <div class="form-group"><label>Address:</label> <input placeholder="Address" class="form-control" type="text" name="address" value="<?php if (isset($user['address']) == true) echo $user['address']; ?>"></div>
+                        <div class="form-group"><label>Mobile:</label> <input placeholder="Mobile phone number" class="form-control" type="text" name="mobile" value="<?php if (isset($user['mobile']) == true) echo $user['mobile']; ?>"></div>
+                        <div class="form-group"><label>Email:</label> <input placeholder="E-mail" class="form-control" type="text" name="email" value="<?php if (isset($user['email']) == true) echo $user['email']; ?>"></div>
+                    <?php if (isset($user['ID_user']) == true): ?>
+                        <div class="form-group"><label>Active:</label> <input class="form-control" type="checkbox-inline" name="active" value="1" <?php if ($user['active']==1) echo 'checked="checked"'; ?>></div>
+                    <?php endif; ?>
+                        <div class="form-group"><label>
+                            Role:</label>
+                            <select name="role" id="">
+                    <?php foreach ($roles as $role): ?>
+                                <option value="<?php echo $role['ID_role']; ?>"><?php echo $role['role']; ?></option>
+                    <?php endforeach; ?>
+                            </select>
+                        </div>
+                        <div class="form-group"><input class="btn btn-primary btn-lg usersubmit" type="submit" value="Submit"></div>
+                    </form>
+                </div>
+    </div>        
+</div>
+
+
+
+
+
 <?php if(isset($notify) == true): ?>
     <div>
         <?php echo $notify; ?>
