e68760ee54afa781fedcfeb648b87ad55ff035b6
tsapina <tosapina@hotmail.com>
2014-01-03 19:12:39 +0100

    app.css

--- application/views/admin/users_list.php
+++ application/views/admin/users_list.php
@@ -1,32 +1,34 @@
 <div class="main-content">
-<?php 
-echo "<pre>";
-var_dump($users); 
-echo "</pre>";
+	<div class="padded">
+		<table class="table table-striped table-bordered table-hover datatable">
+			<thead>
+				<tr>
+				  <th>Username</th>
+				  <th>Firstname</th>
+				  <th>Lastname</th>
+				  <th>Address</th>
+				  <th>Mobile</th>
+				  <th>Email</th>
+				  <th>Role</th>
+				</tr>
+			</thead>
+			<tbody>
+			<?php 
+			foreach ($users as $user) 
+			{
+				echo "<tr>";
+				echo "<th>{$user['username']}</th>";
+				echo "<th>{$user['firstname']}</th>";
+				echo "<th>{$user['lastname']}</th>";
+				echo "<th>{$user['address']}</th>";
+				echo "<th>{$user['mobile']}</th>";
+				echo "<th>{$user['email']}</th>";
+				echo "<th>{$user['role']}</th>";
+				echo "</tr>";
+			}
 
-?>
-
-<table class="table table-striped table-bordered table-hover datatable">
-<thead>
-	<tr>
-	  <th>Username</th>
-	  <th>Firstname</th>
-	  <th>Lastname</th>
-	  <th>Address</th>
-	  <th>Mobile</th>
-	  <th>Email</th>
-	  <th>Role</th>
-	</tr>
-</thead>
-<tbody>
-<?php 
-foreach ($users as $user) 
-{
-	echo "<th>{$user['username']}</th>";
-
-}
-
-?>
-</tbody>
-</table>
+			?>
+			</tbody>
+		</table>
+	</div>
 </div>
